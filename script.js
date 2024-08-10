413ff480ed09c69ecbc32089f2dfa9fac9c700f0
Mutisostud02 <https://github.com/Mutisostud02>
2024-08-11 00:53:10 +0300

    Added get(key) function to return value of a key if present. Added has(key) to return true if key is present. Added remove(key) to remove key and return true if present.

---
+++ script.js
@@ -0,0 +1,69 @@
+function HashMap() {
+    let arr = new Array(16);
+    function hash(key) {
+        let hashCode = 0;
+        let primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (hashCode * primeNumber + key.charCodeAt(i)) % 16;
+        }
+        return hashCode;
+    }
+    function set(key, value) {
+        let index = hash(key);
+        if(!arr[index]) {
+            arr[index] = [];
+        }  
+
+        for(let i = 0; i < arr[index].length; i++) {
+            if (arr[index][i][0] === key) {
+                arr[index][i][1] = value;
+                return true;
+            }
+        }
+        if (arr[index].length > 0) {
+            console.log(`Possible collisions detected at index ${index}`);
+            console.log(`Current bucket state: ${arr[index]}`)
+            return;
+        }
+        arr[index].push([key, value]);
+        return true;
+    }
+    function get(key) {
+        let index = hash(key);
+        if (arr[index] === undefined) {
+            return null;
+        }
+        for(let i = 0; i < arr[index].length; i++) {
+            if (arr[index][i][0] === key) {
+                return arr[index][i][1];
+            }
+        }
+        return null;
+    }
+    function has(key) {
+        let index = hash(key);
+        if (arr[index] === undefined) {
+            return false;
+        }
+        for(let i = 0; i < arr[index].length; i++) {
+            if (arr[index][i][0] === key) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function remove(key) {
+        let index = hash(key);
+        if (arr[index] === undefined) {
+            return false;
+        }
+        for(let i = 0; i < arr[index].length; i++) {
+            if (arr[index][i][0] === key) {
+                arr[index].splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+    return {hash, set, get, has, remove, arr};
+}
